94169f60f2434eedd02b43532883c1786856c997
Jing <sukitta.a@ku.th>
2019-11-01 19:01:12 +0700

    add insurance type data

--- database/seeds/InsuranceTypeSeeder.js
+++ database/seeds/InsuranceTypeSeeder.js
@@ -21,6 +21,7 @@ class InsuranceTypeSeeder {
     // insuranceType.protection_detail_id = 0
     // insuranceType.save()
    
+    //-----2+ max ซ่อมห้าง-------
     insuranceType  = new InsuranceType()
     insuranceType.insurance_name = "2+ Max (ซ่อมห้าง)"
     insuranceType.option = JSON.stringify({
@@ -46,6 +47,110 @@ class InsuranceTypeSeeder {
       "ภัยธรรมชาติ" : "200,000"
     })
     insuranceType.protection_detail_id = 1
+    insuranceType.save()
+
+    //--------2+ max ซ่อมอู่--------
+    insuranceType  = new InsuranceType()
+    insuranceType.insurance_name = "2+ Max (ซ่อมอู่)"
+    insuranceType.option = JSON.stringify({
+      "ทุนประกัน" : "150,000",
+      "ภัยธรรมชาติ" : "100,000"
+    })
+    insuranceType.protection_detail_id = 2
+    insuranceType.save()
+
+    insuranceType  = new InsuranceType()
+    insuranceType.insurance_name = "2+ Max (ซ่อมอู่)"
+    insuranceType.option = JSON.stringify({
+      "ทุนประกัน" : "200,000",
+      "ภัยธรรมชาติ" : "200,000"
+    })
+    insuranceType.protection_detail_id = 2
+    insuranceType.save()
+
+    insuranceType  = new InsuranceType()
+    insuranceType.insurance_name = "2+ Max (ซ่อมอู่)"
+    insuranceType.option = JSON.stringify({
+      "ทุนประกัน" : "300,000",
+      "ภัยธรรมชาติ" : "200,000"
+    })
+    insuranceType.protection_detail_id = 2
+    insuranceType.save()
+
+    //--------2+ 35 up--------
+    insuranceType  = new InsuranceType()
+    insuranceType.insurance_name = "2+ 35 up"
+    insuranceType.option = JSON.stringify({
+      "ทุนประกัน" : "150,000",
+      "ภัยธรรมชาติ" : null
+    })
+    insuranceType.protection_detail_id = 3
+    insuranceType.save()
+
+    insuranceType  = new InsuranceType()
+    insuranceType.insurance_name = "2+ 35 up"
+    insuranceType.option = JSON.stringify({
+      "ทุนประกัน" : "200,000",
+      "ภัยธรรมชาติ" : null
+    })
+    insuranceType.protection_detail_id = 3
+    insuranceType.save()
+
+
+    //-----2+ Minimal ซ่อมอู่-------
+    insuranceType  = new InsuranceType()
+    insuranceType.insurance_name = "2+ Minimal (ซ่อมอู่)"
+    insuranceType.option = null
+    insuranceType.protection_detail_id = 4
+    insuranceType.save()
+
+    //-------------- 3+ --------------
+    insuranceType  = new InsuranceType()
+    insuranceType.insurance_name = "3+"
+    insuranceType.option = JSON.stringify({
+      // "ทุนประกัน" : "150,000",
+      // "ภัยธรรมชาติ" : null
+      "ค่าเสียหายส่วนแรก" : "2000"
+    })
+    insuranceType.protection_detail_id = 5
+    insuranceType.save()
+
+    insuranceType  = new InsuranceType()
+    insuranceType.insurance_name = "3+"
+    insuranceType.option = JSON.stringify({
+      // "ทุนประกัน" : "150,000",
+      // "ภัยธรรมชาติ" : null
+      "ค่าเสียหายส่วนแรก" : "ไม่มี"
+    })
+    insuranceType.protection_detail_id = 5
+    insuranceType.save()
+
+    //-------------- 2 เซฟ Special --------------
+    insuranceType  = new InsuranceType()
+    insuranceType.insurance_name = "2 เซฟ Special"
+    insuranceType.option = null
+    insuranceType.protection_detail_id = 6
+    insuranceType.save()
+
+    //-------------- 2 กันชน --------------
+    insuranceType  = new InsuranceType()
+    insuranceType.insurance_name = "2 กันชน"
+    insuranceType.option = null
+    insuranceType.protection_detail_id = 6
+    insuranceType.save()
+
+    //-------------- 3 Best --------------
+    insuranceType  = new InsuranceType()
+    insuranceType.insurance_name = "3 Best"
+    insuranceType.option = null
+    insuranceType.protection_detail_id = 7
+    insuranceType.save()
+
+    //-------------- 3 กันชน --------------
+    insuranceType  = new InsuranceType()
+    insuranceType.insurance_name = "3 กันชน"
+    insuranceType.option = null
+    insuranceType.protection_detail_id = 8
     insuranceType.save()
   }
 }
