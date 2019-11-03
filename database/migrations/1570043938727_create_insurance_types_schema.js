ae023033e060926734edf9b6d2ae7175befc9c17
chalermchon <chalermchon.o@ku.th>
2019-11-03 16:00:59 +0700

    change insurance_type_migration

--- database/migrations/1570043938727_create_insurance_types_schema.js
+++ database/migrations/1570043938727_create_insurance_types_schema.js
@@ -7,6 +7,7 @@ class InsuranceTypesSchema extends Schema {
   up () {
     this.create('insurance_types', (table) => {
       table.bigIncrements('id')
+      table.string('insurance_group', 20).notNullable()
       table.string('insurance_name', 100).notNullable()
       table.json('option').nullable().defaultTo(null)
       table.bigInteger('protection_detail_id').unsigned().references('id').inTable('protection_details')
